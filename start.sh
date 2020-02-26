8e467b7509072f52ab4f9e0fdf51643294a0aa53
yohan <783b8c87@scimetis.net>
2020-02-26 17:40:13 +0100

    Added mounting volume with openstack.

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ~/openrc.sh
+INSTANCE=$(/home/yohan/env_py3/bin/openstack server show -c id --format value $(hostname))
+mkdir -p /mnt/volumes/onlyoffice_data
+if ! mountpoint -q /mnt/volumes/onlyoffice_data
+then
+     VOLUME_ID=$(/home/yohan/env_py3/bin/openstack volume show onlyoffice_data -c id --format value)
+     test -e /dev/disk/by-id/*${VOLUME_ID:0:20} || nova volume-attach $INSTANCE $VOLUME_ID auto
+     sleep 3
+     sudo mount /dev/disk/by-id/*${VOLUME_ID:0:20} /mnt/volumes/onlyoffice_data
+fi
+mkdir -p /mnt/volumes/onlyoffice_log
+if ! mountpoint -q /mnt/volumes/onlyoffice_log
+then
+     VOLUME_ID=$(/home/yohan/env_py3/bin/openstack volume show onlyoffice_log -c id --format value)
+     test -e /dev/disk/by-id/*${VOLUME_ID:0:20} || nova volume-attach $INSTANCE $VOLUME_ID auto
+     sleep 3
+     sudo mount /dev/disk/by-id/*${VOLUME_ID:0:20} /mnt/volumes/onlyoffice_log
+fi
+
+sudo docker-compose up -d
